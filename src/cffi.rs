a2aa7b62263f3c6751fd3ff91e004119b1ebabd4
mcbegamerxx954 <154642722+mcbegamerxx954@users.noreply.github.com>
2011-02-16 14:00:00 +0100

    Fix

---
+++ src/cffi.rs
@@ -0,0 +1,45 @@
+use core::slice;
+use scroll::Pread;
+use std::mem;
+
+use crate::{CompiledMaterialDefinition, ALL_VERSIONS};
+#[repr(C)]
+#[derive(Debug, Clone, Copy)]
+struct Buffer {
+    data: *mut u8,
+    len: usize,
+}
+
+#[no_mangle]
+/// Update a material file
+/// # Safety
+/// - Input pointer and length are valid
+/// - You free the output later
+extern "C" fn update_file(in_buffer: Buffer, out_buffer: *mut Buffer) -> libc::c_int {
+    let slice = unsafe { slice::from_raw_parts(in_buffer.data, in_buffer.len) };
+    let mut output = Vec::with_capacity(slice.len());
+    for version in ALL_VERSIONS {
+        if let Ok(parsed) = slice.pread_with::<CompiledMaterialDefinition>(0, version) {
+            if let Err(_) = parsed.write(&mut output, crate::MinecraftVersion::V1_21_20) {
+                continue;
+            }
+            let mut boxed = output.into_boxed_slice();
+            let bufdata = Buffer {
+                data: boxed.as_mut_ptr(),
+                len: boxed.len(),
+            };
+            unsafe { *out_buffer = bufdata };
+            std::mem::forget(boxed);
+            return 0;
+        }
+    }
+    return -1;
+}
+#[no_mangle]
+extern "C" fn free_buf(buf: Buffer) {
+    let s = unsafe { std::slice::from_raw_parts_mut(buf.data, buf.len) };
+    let s = s.as_mut_ptr();
+    unsafe {
+        drop(Box::from_raw(s));
+    }
+}
